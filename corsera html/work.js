287cdb262906b55d1b1024fd2d0425f7aef9ba19
adithyacs07 <111640341+adithyacs07@users.noreply.github.com>
2022-10-16 18:37:42 +0530

    Add files via upload

--- corsera html/work.js
+++ corsera html/work.js
@@ -5,10 +5,14 @@ const calculate = document.getElementById("calculate")
 const result = document.getElementById("result")
 
 calculate.addEventListener("click", () => {
+    if  (amount.value < 0) {
+        alert("Enter the positive value")
+        return
+    }
     const simpleInterest = (amount.value * intrestRate.value * years.value)/100
     result.innerHTML=`
     <p>If you deposit ${amount.value}</p>
     <p>at an interest rate of ${intrestRate.value}%</p>
-    <p>You will receive an amount of ${simpleInterest}</p>
+    <p>You will receive an amount of ${simpleInterest + amount.value}</p>
     <p>In the year ${2022 + parseInt(years.value)}</p>`
 })
